bfb5b55299c9eaaa957a2b3c107d7f87d49b0d35
viennabai <44930294+viennabai@users.noreply.github.com>
2020-03-03 15:18:12 -0800

    Update and rename AdventureWorks to AdventureWorks.sql

--- AdventureWorks.sql
+++ AdventureWorks.sql
@@ -1,3 +1,13 @@
+-- #6
+/*
+A "Single Item Order" is a customer order where only one item is ordered. Show the SalesOrderID and the UnitPrice for every Single Item Order.
+*/
+SELECT SalesOrderID, UnitPrice, SUM(OrderQty)
+FROM SalesOrderDetail
+WHERE OrderQty=1
+GROUP BY SalesOrderID 
+HAVING SUM(OrderQty)=1
+
 -- #7
 /*
 Where did the racing socks go? List the product name and the CompanyName for all Customers who ordered ProductModel 'Racing Socks'.
