daf32c090701372e14bf0bc1485fa1572a73801c
viennabai <44930294+viennabai@users.noreply.github.com>
2020-03-04 09:14:40 -0800

    Update AdventureWorks.sql

--- AdventureWorks.sql
+++ AdventureWorks.sql
@@ -21,3 +21,60 @@ FROM ProductModel JOIN Product
      JOIN Customer ON Customer.CustomerID=SalesOrderHeader.CustomerID
 
 WHERE ProductModel.name='Racing Socks'
+
+-- #8
+/*
+Show the product description for culture 'fr' for product with ProductID 736.
+*/
+
+SELECT ProductDescription.DESCRIPTION 
+
+FROM Product JOIN ProductModelProductDescription 
+ON ProductModelProductDescription.ProductModelID=Product.ProductModelID 
+JOIN ProductDescription ON ProductDescription.ProductDescriptionID=ProductModelProductDescription.ProductDescriptionID
+
+WHERE ProductID=736 AND Culture='fr'
+
+-- #9
+/*
+Use the SubTotal value in SaleOrderHeader to list orders from the largest to the smallest. For each order show the CompanyName and the SubTotal and the total weight of the order.
+*/
+
+SELECT ProductDescription.DESCRIPTION 
+
+FROM Product JOIN ProductModelProductDescription 
+ON ProductModelProductDescription.ProductModelID=Product.ProductModelID 
+JOIN ProductDescription ON ProductDescription.ProductDescriptionID=ProductModelProductDescription.ProductDescriptionID
+
+WHERE ProductID=736 AND Culture='fr'
+
+SELECT CompanyName, SubTotal, SUM(OrderQty*Weight) AS TotalWeight
+FROM SalesOrderDetail JOIN Product 
+ON Product.ProductID=SalesOrderDetail.ProductID 
+JOIN SalesOrderHeader 
+ON SalesOrderDetail.SalesOrderID=SalesOrderHeader.SalesOrderID
+JOIN Customer 
+ON SalesOrderHeader.CustomerID=Customer.CustomerID
+GROUP BY SalesOrderDetail.SalesOrderID
+ORDER BY SubTotal DESC 
+
+-- #10
+/*
+How many products in ProductCategory 'Cranksets' have been sold to an address in 'London'?
+*/
+
+SELECT COUNT(DISTINCT SalesOrderDetail.ProductID)
+FROM SalesOrderDetail 
+JOIN Product
+ON SalesOrderDetail.ProductID = Product.ProductID
+JOIN ProductCategory
+ON ProductCategory.ProductCategoryID=Product.ProductCategoryID
+JOIN SalesOrderHeader 
+ON SalesOrderHeader.SalesOrderID=SalesOrderDetail.SalesOrderID
+JOIN CustomerAddress
+ON CustomerAddress.CustomerID=SalesOrderHeader.CustomerID
+JOIN Address 
+ON Address.AddressID=CustomerAddress.AddressID 
+
+WHERE Address.City='London' AND ProductCategory.Name='Cranksets'
+
